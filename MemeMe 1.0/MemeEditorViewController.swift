facfdc04d5a01a66b5eb48606468bab193ca2a91
mitul jindal <mituljindal@gmail.com>
2017-07-27 21:07:05 +0530

    Added Text Fields

--- MemeMe 1.0/MemeEditorViewController.swift
+++ MemeMe 1.0/MemeEditorViewController.swift
@@ -12,14 +12,34 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    let textFieldDelegate = TextFieldDelegate()
+    
+    let memeTextAttributes: [String:Any] = [
+        NSStrokeWidthAttributeName: -3.0,
+        NSForegroundColorAttributeName: UIColor.white,
+        NSStrokeColorAttributeName: UIColor.black,
+        NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!
+    ]
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        topTextField.delegate = textFieldDelegate
+        bottomTextField.delegate = textFieldDelegate
+        topTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        topTextField.textAlignment = .center
+        bottomTextField.textAlignment = .center
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        topTextField.text = "TOP"
+        bottomTextField.text = "BOTTOM"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -43,7 +63,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
-        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imagePickerView.image = image
             imagePickerView.contentMode = UIViewContentMode.scaleAspectFit
         }
---
+++ MemeMe 1.0/TextFieldDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  TextFieldDelegate.swift
+//  MemeMe 1.0
+//
+//  Created by Mitul Jindal on 27/07/17.
+//  Copyright © 2017 mitul jindal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == "TOP" || textField.text == "BOTTOM" {
+            textField.text = ""
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.resignFirstResponder()
+    }
+    
+}
