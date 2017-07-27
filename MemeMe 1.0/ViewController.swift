b8afa2b67a56e46684bc233362558fb28c296af2
mitul jindal <mituljindal@gmail.com>
2017-07-27 14:47:29 +0530

    Image Picker Toolbar

--- MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func pickImage(_ sender: Any) {
+        
+        let pickerController = UIImagePickerController()
+        self.present(pickerController, animated: true, completion: nil)
     }
-
 
 }
 
