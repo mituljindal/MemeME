2c437d4a5cd0be00ff034591d10e7a985f922cf9
mitul jindal <mituljindal@gmail.com>
2017-07-28 12:57:21 +0530

    final

---
+++ MemeMe 1.0/MemeEditor.swift
@@ -0,0 +1,18 @@
+//
+//  MemeModel.swift
+//  MemeMe
+//
+//  Created by Mitul Jindal on 27/07/17.
+//  Copyright © 2017 mitul jindal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topString: String
+    var bottomString: String
+    var originalImage: UIImage
+    var finalMeme: UIImage
+}
+
--- MemeMe 1.0/MemeEditorViewController.swift
+++ MemeMe 1.0/MemeEditorViewController.swift
@@ -14,19 +14,26 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var toolbar: UIToolbar!
+    @IBOutlet weak var navbar: UINavigationBar!
+    @IBOutlet weak var photoButton: UIBarButtonItem!
     
     let textFieldDelegate = TextFieldDelegate()
+    // let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
     
     let memeTextAttributes: [String:Any] = [
         NSStrokeWidthAttributeName: -3.0,
         NSForegroundColorAttributeName: UIColor.white,
         NSStrokeColorAttributeName: UIColor.black,
-        NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!
+        NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 75)!
     ]
     
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+       // toolbar.items = [space, cameraButton, space, photoButton, space]
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
         topTextField.delegate = textFieldDelegate
         bottomTextField.delegate = textFieldDelegate
@@ -34,13 +41,34 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         bottomTextField.defaultTextAttributes = memeTextAttributes
         topTextField.textAlignment = .center
         bottomTextField.textAlignment = .center
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         topTextField.text = "TOP"
         bottomTextField.text = "BOTTOM"
+        shareButton.isEnabled = false
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            if topTextField.isFirstResponder && touch.view != topTextField {
+                topTextField.resignFirstResponder()
+            } else if bottomTextField.isFirstResponder && touch.view != bottomTextField {
+                    bottomTextField.resignFirstResponder()
+            }
+        }
+        super.touchesBegan(touches, with: event)
     }
 
     @IBAction func pickImageFromAlbum(_ sender: Any) {
@@ -60,14 +88,84 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(imagePicker, animated: true, completion: nil)
     }
     
-    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            shareButton.isEnabled = true
             imagePickerView.image = image
             imagePickerView.contentMode = UIViewContentMode.scaleAspectFit
         }
         dismiss(animated: true, completion: nil)
     }
+    
+    func keyboardWillShow(_ notification:Notification) {
+        
+        view.frame.origin.y = 0 - getKeyboardHeight(notification)
+    }
+    
+    func keyboardWillHide(_ notification:Notification) {
+        
+        view.frame.origin.y = 0
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func generateMemedImage() -> UIImage {
+        toolbar.isHidden = true
+        navbar.isHidden = true
+        
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let finalMeme: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        toolbar.isHidden = false
+        navbar.isHidden = false
+        
+        return finalMeme
+    }
 
+    @IBAction func shareMeme(_ sender: Any) {
+        
+        let meme = generateMemedImage()
+        
+        let controller = UIActivityViewController(activityItems: [meme], applicationActivities: nil)
+        controller.completionWithItemsHandler = {
+            (_,successful,_,_) in
+            if successful {
+                UIImageWriteToSavedPhotosAlbum(meme, self, nil /* #selector(self.image(_:didFinishSavingWithError:contextInfo:)) */, nil)
+            }
+        }
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+    func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
 }
